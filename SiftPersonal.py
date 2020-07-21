95625258a7b6f9c030a67163c72459be53658f2f
Matthias Elliott <komizutama@gmail.com>
2020-07-21 17:44:27 -0400

    almost there weird indent error

---
+++ SiftPersonal.py
@@ -0,0 +1,26 @@
+import csv
+import pyyaml
+
+trimmed_list = []
+
+with open('personal_email_domains.yaml') as f:
+    personal_email_domains = yaml.load(f, Loader=yaml.FullLoader)
+
+with open('subscriber_list.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if row['\xef\xbb\xbfEmail'].split('@')[1] not in personal_email_domains:
+            trimmed_list.append(row)
+
+for subscriber in trimmed_list:
+    print subscriber
+    subscriber['Email'] = subscriber.pop('\xef\xbb\xbfEmail')
+    print subscriber
+
+with open('trimmed_list.csv'), mode='w') as csv_file:
+	fieldnames = ['Subscribed', 'Clicked', 'Opened', 'Sent', 'Email']
+	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+	writer.writeheader()
+	writer.writerows(trimmed_list)
