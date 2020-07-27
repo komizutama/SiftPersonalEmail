c5e1ff79988fbd3b5b5d8f09bd192a99490c9e56
Matthias Elliott <komizutama@gmail.com>
2020-07-27 19:20:03 -0400

    adapted to create jp list, discard list an ignore caps. needs a refactor

--- SiftPersonal.py
+++ SiftPersonal.py
@@ -2,24 +2,62 @@ import csv
 import yaml
 
 trimmed_list = []
+discarded_list = []
+jp_list = []
+
+
 
 with open('personal_email_domains.yaml') as f:
-    personal_email_domains = yaml.load(f, Loader=yaml.FullLoader)
+    personal_email_domains = set(yaml.load(f, Loader=yaml.FullLoader))
+    print personal_email_domains
+
+with open('domains2ignore.yaml') as f:
+    unwanted_tlds = yaml.load(f, Loader = yaml.FullLoader)
+    print unwanted_tlds
 
 with open('subscriber_list.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
     for row in csv_reader:
-        if row['\xef\xbb\xbfEmail'].split('@')[1] not in personal_email_domains:
+        domain_name = row['\xef\xbb\xbfEmail'].split('@')[1]
+        lower_domain_name = domain_name.lower()
+        tld = lower_domain_name.split('.')
+
+        if tld[-1] == 'jp':
+            jp_list.append(row)
+            print "added to JP"
+        elif set(tld).intersection(personal_email_domains) or tld[-1] in unwanted_tlds:
+            discarded_list.append(row)
+            print "added to discard"
+        else:
             trimmed_list.append(row)
+            print "added to main"
+
+
 
 for subscriber in trimmed_list:
-    print subscriber
     subscriber['Email'] = subscriber.pop('\xef\xbb\xbfEmail')
-    print subscriber
+
+for subscriber in discarded_list:
+    subscriber['Email'] = subscriber.pop('\xef\xbb\xbfEmail')
+
+for subscriber in jp_list:
+    subscriber['Email'] = subscriber.pop('\xef\xbb\xbfEmail')
 
 with open('trimmed_list.csv', mode='w') as csv_file:
     fieldnames = ['Subscribed', 'Clicked', 'Opened', 'Sent', 'Email']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerows(trimmed_list)
+
+with open('discard_list.csv', mode='w') as csv_file:
+    fieldnames = ['Subscribed', 'Clicked', 'Opened', 'Sent', 'Email']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(discarded_list)
+
+with open('jp_list.csv', mode='w') as csv_file:
+    fieldnames = ['Subscribed', 'Clicked', 'Opened', 'Sent', 'Email']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(jp_list)
